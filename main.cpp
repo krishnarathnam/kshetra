6138a24bf6dc7a8b1d5f66cc5dd2ef02a53a7811
Krishna Rathnam <kr130205@gmail.com>
2026-01-20 16:16:50 +0530

    main

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using U64 = unsigned long long;
+
+// clang-format off
+enum Square {
+  a8, b8, c8, d8, e8, f8, g8, h8,
+  a7, b7, c7, d7, e7, f7, g7, h7,
+  a6, b6, c6, d6, e6, f6, g6, h6,
+  a5, b5, c5, d5, e5, f5, g5, h5,
+  a4, b4, c4, d4, e4, f4, g4, h4,
+  a3, b3, c3, d3, e3, f3, g3, h3,
+  a2, b2, c2, d2, e2, f2, g2, h2,
+  a1, b1, c1, d1, e1, f1, g1, h1
+};
+// clang-format on
+
+// set/get/pop macros
+#define set_bit(bitboard, square) (bitboard |= (1ULL << square))
+#define pop_bit(bitboard, square)                                              \
+  set_bit(bitboard, square) ? bitboard ^= (1ULL << square) : 0
+#define get_bit(bitboard, square) (bitboard & (1ULL << square))
+
+void print_bitboard(U64 bitboard) {
+  std::cout << "\n";
+  for (int rank = 0; rank < 8; rank++) {
+    for (int file = 0; file < 8; file++) {
+      int square = rank * 8 + file;
+
+      if (!file) {
+        std::cout << (8 - rank) << " ";
+      }
+
+      std::cout << (get_bit(bitboard, square) ? 1 : 0) << " ";
+    }
+    std::cout << "\n";
+  }
+
+  std::cout << "  a b c d e f g h\n\n";
+  std::cout << "Bitboard: " << bitboard << std::endl;
+  ;
+}
+
+int main() {
+  U64 bitboard = 0ULL;
+  set_bit(bitboard, e4);
+  set_bit(bitboard, c2);
+  set_bit(bitboard, d5);
+  print_bitboard(bitboard);
+
+  pop_bit(bitboard, e4);
+  print_bitboard(bitboard);
+
+  return 0;
+}
